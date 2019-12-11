a0a474e1455dcbd7632d122de8ae6712d612cf10
JohnCharamonde <john.charamonde@gmail.com>
2019-12-10 23:26:36 -0500

    General logic complete for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,39 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+var stringifyJSON = function (obj) {
+  var brackets = ['[', ']'];
+  var braces = ['{', '}'];
+  var str = '';
+  var keys = [];
+  var arrayStr = '';
+  var objStr;
 
-// but you don't so you're going to write it from scratch:
-
-var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    str += 'null';
+    return str;
+  } else if (obj === undefined) {
+    str += 'undefined';
+    return str;
+  } else if (typeof (obj) === 'string' || typeof (obj) === 'number' || typeof (obj) === boolean) {
+    str += obj;
+    return str;
+  } else if (Array.isArray(obj)) {
+    arrayStr = ''
+    for (let i = 0; i < obj.length; i++) {
+      if (i === obj.length - 1) {
+        arrayStr += obj[i];
+      } else {
+        arrayStr + obj[i] + ','
+      }
+      arrayStr = '[' + arrayStr + ']';
+      str += arrayStr;
+      return str;
+    }
+  } else if (typeof (obj) === 'object') {
+    objStr = ''
+    for (var key in obj) {
+      objStr += key += ':' += obj[key];
+    }
+    objStr = '{' + objStr + '}';
+    str += objStr;
+    return str;
+  }
 };
